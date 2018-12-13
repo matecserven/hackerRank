d2b15bb9f98873854a7e5be2a8415c0ba9486b49
Cserven Máté <cserk5@gmail.com>
2018-12-13 11:05:38 +0100

    Compare the triplets Done

---
+++ compareTheTriplets.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const numbersOfJohn = [1, 2, 3];
+const numbersOfBob = [2, 2, 2];
+
+const compareTriplets = (arrayA, arrayB) => {
+  let score = [0, 0]
+  
+  arrayA.forEach((elem, index) => {
+    if (elem > arrayB[index]) {
+      score[0]++;
+    } else if (elem < arrayB[index]) {
+      score[1]++;
+    }
+  });
+
+  return score;
+}
+
+console.log(compareTriplets(numbersOfJohn, numbersOfBob));
